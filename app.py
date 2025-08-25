9928dc4f33ceb7bd44b5685d7b1c649f990dec85
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-08-25 17:29:01 +0530

    integrated the pyttsx3 library, but it does not speak after the first time.

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 import pygame
 import os
 import speech_recognition as sr
+import pyttsx3
 
 # Init mixer
 pygame.mixer.init() #initializes the pygame mixer, for using play and pause commands
+
+engine = pyttsx3.init() #initializes the pyttsx3.
+engine.setProperty('rate', 160)
 
 songsfolder = "songs"
 playlist = [f for f in os.listdir(songsfolder) if f.endswith(".mp3")]
 current = 0 #current index of the song from playlist.
 
+def speak(text):
+    print("🔊 " + text)
+    
+    engine.say(text)
+    engine.runAndWait()
+
+
 def play_song(index):
-    pygame.mixer.music.load(os.path.join(songsfolder, playlist[index]))
+    song = playlist[index]
+    pygame.mixer.music.load(os.path.join(songsfolder, song))
     pygame.mixer.music.play()
-    print(f"Now playing: {playlist[index]}")
+    speak(f"Now playing: {song}")
 
 def get_voice_command():
     r = sr.Recognizer() 
@@ -37,20 +49,26 @@ while True:
 
     if "pause" in command:
         pygame.mixer.music.pause()
+        speak("Music paused.")
     elif "resume" in command or "play" in command:
         pygame.mixer.music.unpause()
+        speak("Resuming music.")
     elif "next" in command:
         current = (current+1) % len(playlist)
         play_song(current)
+        # speak("")
     elif "previous" in command or "back" in command:
         current = (current-1) % len(playlist)
         play_song(current)
     elif "stop" in command:
         pygame.mixer.music.stop()
+        speak("Stopping music.")        
+
     elif "exit" in command or "quit" in command:
         pygame.mixer.music.stop()
+        speak("Exiting the music player.")
         break
     else:
-        print("Did not catch a valid command.")
+        print("(Did not catch a valid command.)")
 
 
